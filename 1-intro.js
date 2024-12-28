b547231f7269b870f035be71e8b88c0a8c399aae
Ernest <kolecryp@gmail.com>
2024-12-28 13:14:26 +0100

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,7 @@
+const amount =11
+if(amount <10){
+    console.log("Small Number")
+}else{
+    console.log("Large Number")
+}
+console.log("It's my first App!")
---
+++ 4-names.js
@@ -0,0 +1,7 @@
+//local
+const secret ="SUPER SECRETE"
+//share
+const mercy="Mercy"
+const john="John"
+
+module.exports={john,mercy};
